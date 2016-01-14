e04a7234592a14e8ae0339123b4c8da7bbaf7b0e
S. Davilla <davilla@4pi.com>
2016-01-14 12:18:01 -0500

    cleanup, stray bits that should have been removed

--- xbmc/platform/darwin/DarwinUtils.h
+++ xbmc/platform/darwin/DarwinUtils.h
@@ -30,8 +30,6 @@ typedef const struct __CFString * CFStringRef;
 class CDarwinUtils
 {
 public:
-  static CDarwinUtils &GetInstance();
-  
   static const char *getIosPlatformString(void);
   static bool        IsMavericks(void);
   static bool        IsSnowLeopard(void);
