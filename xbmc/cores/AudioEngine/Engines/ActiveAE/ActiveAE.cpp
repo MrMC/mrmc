d2c41d8bc1c860d49135afc3bcf03728b87bf436
S. Davilla <davilla@4pi.com>
2016-07-17 16:53:40 -0400

    changed, allow droid to respond to AE/IDispResource callbacks

--- xbmc/cores/AudioEngine/Engines/ActiveAE/ActiveAE.cpp
+++ xbmc/cores/AudioEngine/Engines/ActiveAE/ActiveAE.cpp
@@ -297,9 +297,7 @@ CActiveAE::~CActiveAE()
 
 void CActiveAE::Dispose()
 {
-#if defined(HAS_GLX) || defined(TARGET_DARWIN)
   g_Windowing.Unregister(this);
-#endif
 
   m_bStop = true;
   m_outMsgEvent.Set();
@@ -2608,9 +2606,7 @@ bool CActiveAE::Initialize()
   }
 
   // hook into windowing for receiving display reset events
-#if defined(HAS_GLX) || defined(TARGET_DARWIN) 
   g_Windowing.Register(this);
-#endif
 
   m_inMsgEvent.Reset();
   return true;
