01d8511f9c745b9292122cb31b8f2a926e74ec98
S. Davilla <davilla@4pi.com>
2016-07-20 12:27:10 -0400

    Revert "upstream sync, fixup stalls after seeks"
    This reverts commit 88afc795ca4575b5e08340efda86ea7fa901f225.

--- xbmc/cores/dvdplayer/DVDPlayerVideo.cpp
+++ xbmc/cores/dvdplayer/DVDPlayerVideo.cpp
@@ -1090,8 +1090,7 @@ int CDVDPlayerVideo::OutputPicture(const DVDVideoPicture* src, double pts)
   }
 
   // make sure waiting time is not negative
-  // and do not stall after seeks
-  int maxWaitTime = std::min(std::max(DVD_TIME_TO_MSEC(iSleepTime) + 500, 50), 500);
+  int maxWaitTime = std::max(DVD_TIME_TO_MSEC(iSleepTime) + 500, 50);
   // don't wait when going ff
   if (m_speed > DVD_PLAYSPEED_NORMAL)
     maxWaitTime = std::max(DVD_TIME_TO_MSEC(iSleepTime), 0);
