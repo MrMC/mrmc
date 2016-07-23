a3d3a342efa949731fe5439847da1c34a2101246
phate89 <phates89@gmail.com>
2016-07-23 16:08:50 +0200

    Double the backslash in mysql in case we're in 'LIKE'

--- xbmc/dbwrappers/mysqldataset.cpp
+++ xbmc/dbwrappers/mysqldataset.cpp
@@ -810,12 +810,13 @@ void MysqlDatabase::mysqlVXPrintf(
   length = 0;
   bufpt = 0;
   for(; (c=(*fmt))!=0; ++fmt){
+    bool isLike = false;
     if( c!='%' ){
       int amt;
       bufpt = (char *)fmt;
       amt = 1;
       while( (c=(*++fmt))!='%' && c!=0 ) amt++;
-      mysqlStrAccumAppend(pAccum, bufpt, amt);
+      isLike = mysqlStrAccumAppend(pAccum, bufpt, amt);
       if( c==0 ) break;
     }
     if( (c=(*++fmt))==0 ){
@@ -1180,7 +1181,11 @@ void MysqlDatabase::mysqlVXPrintf(
         int needQuote;
         char ch;
         char q = ((xtype==etSQLESCAPE3)?'"':'\'');   /* Quote character */
-        const char *escarg = va_arg(ap, char*);
+        std::string arg = va_arg(ap, char*);
+        if (isLike)
+          StringUtils::Replace(arg, "\\", "\\\\");
+        const char *escarg = arg.c_str();
+
         isnull = escarg==0;
         if( isnull ) escarg = (xtype==etSQLESCAPE2 ? "NULL" : "(NULL)");
         k = precision;
@@ -1243,15 +1248,15 @@ void MysqlDatabase::mysqlVXPrintf(
 /*
 ** Append N bytes of text from z to the StrAccum object.
 */
-void MysqlDatabase::mysqlStrAccumAppend(StrAccum *p, const char *z, int N) {
+bool MysqlDatabase::mysqlStrAccumAppend(StrAccum *p, const char *z, int N) {
   if( p->tooBig | p->mallocFailed ){
-    return;
+    return false;
   }
   if( N<0 ){
     N = strlen(z);
   }
   if( N==0 || z==0 ){
-    return;
+    return false;
   }
   if( p->nChar+N >= p->nAlloc ){
     char *zNew;
@@ -1260,7 +1265,7 @@ void MysqlDatabase::mysqlStrAccumAppend(StrAccum *p, const char *z, int N) {
     if( szNew > p->mxAlloc ){
       mysqlStrAccumReset(p);
       p->tooBig = 1;
-      return;
+      return false;
     }else{
       p->nAlloc = szNew;
     }
@@ -1272,11 +1277,22 @@ void MysqlDatabase::mysqlStrAccumAppend(StrAccum *p, const char *z, int N) {
     }else{
       p->mallocFailed = 1;
       mysqlStrAccumReset(p);
-      return;
+      return false;
     }
   }
+
+  bool isLike = false;
+  std::string testString(z, N);
+  if (testString.find("LIKE") != std::string::npos || testString.find("like") != std::string::npos)
+  {
+    CLog::Log(LOGDEBUG, "This query part contains a like, we will double backslash in the next field: %s", testString.c_str());
+    isLike = true;
+
+  }
+
   memcpy(&p->zText[p->nChar], z, N);
   p->nChar += N;
+  return isLike;
 }
 
 /*
