85bb93b9ca138ef5b61e26a148605d3c938d38d9
S. Davilla <davilla@4pi.com>
2016-11-14 13:04:00 -0500

    fixed, mem/cpu usage logging under droid

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -4394,22 +4394,36 @@ void CApplication::Process()
 // We get called every 500ms
 void CApplication::ProcessSlow()
 {
-#if defined(TARGET_DARWIN)
   if (CSettings::GetInstance().GetBool(CSettings::SETTING_DEBUG_MEMORYCPU))
   {
     static int skipcount = 0;
     if (skipcount++ > 3)
     {
       skipcount = 0;
-      int64_t free, delta;
-      CDarwinUtils::GetAppMemory(free, delta);
+#if defined(TARGET_ANDROID)
+      int max_percent = 100;
+#else
+      int max_percent = 100 * g_cpuInfo.getCPUCount();
+#endif
+
+      int64_t used, delta;
+      static int64_t old_used = 0;
+#if defined(TARGET_DARWIN)
+      CDarwinUtils::GetAppMemory(used, delta);
+#else
+      MEMORYSTATUSEX stat;
+      stat.dwLength = sizeof(MEMORYSTATUSEX);
+      GlobalMemoryStatusEx(&stat);
+      used = stat.ullTotalPhys - stat.ullAvailPhys;
+#endif
+      delta = used - old_used;
+      old_used = used;
 
       const double unit = 1024.0 * 1024.0;
       CLog::Log(LOGDEBUG, "CPU used = %d/%d percent, App Memory: %4.2f MBs, delta = %lld bytes",
-        g_cpuInfo.getUsedPercentage(), 100 * g_cpuInfo.getCPUCount(), (double)free/unit, delta);
-    }
+        g_cpuInfo.getUsedPercentage(), max_percent, (double)used/unit, delta);
+      }
   }
-#endif
 
   g_powerManager.ProcessEvents();
 
