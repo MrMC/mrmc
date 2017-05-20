f24493a55edfc6dd6e47d83f388bcc7ae4d0fd50
amet <amet.nospam@gmail.com>
2017-05-20 16:45:17 +0400

    smb: remove FORCEV1 setting, does nothing as we only support v1 :)

--- xbmc/settings/Settings.h
+++ xbmc/settings/Settings.h
@@ -304,7 +304,6 @@ public:
   static const std::string SETTING_SMB_WINSSERVER;
   static const std::string SETTING_SMB_WORKGROUP;
   static const std::string SETTING_SMB_ENABLEDSM;
-  static const std::string SETTING_SMB_FORCEV1;
   static const std::string SETTING_SMB_OVERWRITECONF;
   static const std::string SETTING_SMB_STATFILES;
   static const std::string SETTING_SMB_CLIENTTIMEOUT;
