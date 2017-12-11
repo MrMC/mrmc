7e30bd67d31482c1c81b481678ff0d735cc3a38f
S. Davilla <davilla@4pi.com>
2017-12-11 17:16:52 -0500

    fixed, osx build for DVDVideoCodecVDA

--- xbmc/cores/dvdplayer/DVDCodecs/Video/DVDVideoCodecVDA.cpp
+++ xbmc/cores/dvdplayer/DVDCodecs/Video/DVDVideoCodecVDA.cpp
@@ -477,8 +477,8 @@ bool CDVDVideoCodecVDA::GetPicture(DVDVideoPicture* pDvdVideoPicture)
 
     pDvdVideoPicture->format          = RENDER_FMT_CVBREF;
     pDvdVideoPicture->iFlags          = DVP_FLAG_ALLOCATED;
-    pDvdVideoPicture->color_range     = m_hints.colorrange;
-    pDvdVideoPicture->color_matrix    = m_hints.colorspace;
+    //pDvdVideoPicture->color_range     = m_hints.colorrange;
+    //pDvdVideoPicture->color_matrix    = m_hints.colorspace;
     pDvdVideoPicture->iWidth          = CVPixelBufferGetWidth(pDvdVideoPicture->cvBufferRef);
     pDvdVideoPicture->iHeight         = CVPixelBufferGetHeight(pDvdVideoPicture->cvBufferRef);
     pDvdVideoPicture->iDisplayWidth   = pDvdVideoPicture->iWidth;
