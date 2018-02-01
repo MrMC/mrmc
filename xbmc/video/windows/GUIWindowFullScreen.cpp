MrMC/mrmc-private

core: add CGUIDialogOSDSettings window

xbmc/video/windows/GUIWindowFullScreen.cpp
@@ -139,6 +139,10 @@ bool CGUIWindowFullScreen::OnAction(const CAction &action)
     ToggleOSD();
     return true;
 
+  case ACTION_SHOW_OSD_SETTINGS:
+    ToggleOSDSettings();
+    return true;
+      
   case ACTION_TRIGGER_OSD:
     TriggerOSD();
     return true;
@@ -663,6 +667,19 @@ void CGUIWindowFullScreen::ToggleOSD()
     }
   }
 
+  MarkDirtyRegion();
+}
+
+void CGUIWindowFullScreen::ToggleOSDSettings()
+{
+  CGUIDialogVideoOSD *pOSD = (CGUIDialogVideoOSD *)g_windowManager.GetWindow(WINDOW_DIALOG_OSD_SETTINGS);
+  if (pOSD)
+  {
+    if (pOSD->IsDialogRunning())
+      pOSD->Close();
+    else
+      pOSD->Open();
+  }
   MarkDirtyRegion();
 }
 

MrMC/mrmc

xbmc/video/windows/GUIWindowFullScreen.cpp
@@ -139,6 +139,10 @@ bool CGUIWindowFullScreen::OnAction(const CAction &action)
     ToggleOSD();
     return true;
 
+  case ACTION_SHOW_OSD_SETTINGS:
+    ToggleOSDSettings();
+    return true;
+
   case ACTION_TRIGGER_OSD:
     TriggerOSD();
     return true;
@@ -676,4 +680,17 @@ void CGUIWindowFullScreen::TriggerOSD()
       pOSD->SetAutoClose(iSec * 1000);
     pOSD->Open();
   }
+}
+
+void CGUIWindowFullScreen::ToggleOSDSettings()
+{
+  CGUIDialog *pOSD = (CGUIDialog*)g_windowManager.GetWindow(WINDOW_DIALOG_OSD_SETTINGS);
+  if (pOSD)
+  {
+    if (pOSD->IsDialogRunning())
+      pOSD->Close();
+    else
+      pOSD->Open();
+  }
+  MarkDirtyRegion();
 }