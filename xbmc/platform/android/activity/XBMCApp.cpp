koying/SPMC

CHG: [droid] better droid apps launcher

xbmc/platform/android/activity/XBMCApp.cpp
@@ -38,6 +38,7 @@
 #include <androidjni/BroadcastReceiver.h>
 #include <androidjni/Build.h>
 #include <androidjni/CharSequence.h>
+#include <androidjni/ComponentName.h>
 #include <androidjni/ConnectivityManager.h>
 #include <androidjni/ContentResolver.h>
 #include <androidjni/Context.h>
@@ -53,6 +54,7 @@
 #include <androidjni/NetworkInfo.h>
 #include <androidjni/PackageManager.h>
 #include <androidjni/PowerManager.h>
+#include <androidjni/ResolveInfo.h>
 #include <androidjni/StatFs.h>
 #include <androidjni/System.h>
 #include <androidjni/SystemClock.h>
@@ -846,23 +848,32 @@ std::vector<androidPackage> CXBMCApp::GetApplications()
   CSingleLock lock(m_applicationsMutex);
   if (m_applications.empty())
   {
-    CJNIList<CJNIApplicationInfo> packageList = GetPackageManager().getInstalledApplications(CJNIPackageManager::GET_ACTIVITIES);
-    int numPackages = packageList.size();
+    CJNIIntent main(CJNIIntent::ACTION_MAIN, CJNIURI());
+    main.addCategory(CJNIIntent::CATEGORY_LAUNCHER);
+
+    CJNIList<CJNIResolveInfo> launchables = GetPackageManager().queryIntentActivities(main, 0);
+    int numPackages = launchables.size();
     for (int i = 0; i < numPackages; i++)
     {
-      CJNIIntent intent;
-      if (CAndroidFeatures::IsLeanback())
-        intent = GetPackageManager().getLeanbackLaunchIntentForPackage(packageList.get(i).packageName);
-      if (!intent)
-        intent = GetPackageManager().getLaunchIntentForPackage(packageList.get(i).packageName);
-      if (!intent)
-        continue;
+      CJNIResolveInfo launchable = launchables.get(i);
+      CJNIActivityInfo activity = launchable.activityInfo;
+
+//      CJNIIntent intent;
+//      if (CAndroidFeatures::IsLeanback())
+//        intent = GetPackageManager().getLeanbackLaunchIntentForPackage(packageList.get(i).packageName);
+//      if (!intent)
+//        intent = GetPackageManager().getLaunchIntentForPackage(packageList.get(i).packageName);
+//      if (!intent)
+//        continue;
 
       androidPackage newPackage;
-      newPackage.packageName = packageList.get(i).packageName;
-      newPackage.packageLabel = GetPackageManager().getApplicationLabel(packageList.get(i)).toString();
-      newPackage.icon = packageList.get(i).icon;
+      newPackage.packageName = activity.applicationInfo.packageName;
+      newPackage.className = activity.name;
+      newPackage.packageLabel = launchable.loadLabel(GetPackageManager()).toString();
+      newPackage.icon = activity.applicationInfo.icon;
       m_applications.push_back(newPackage);
+
+      CLog::Log(LOGDEBUG, "%s - p:%s c:%s pl:%s i:%d", __FUNCTION__, newPackage.packageName.c_str(), newPackage.className.c_str(), newPackage.packageLabel.c_str(), newPackage.icon);
     }
   }
 
@@ -874,9 +885,42 @@ bool CXBMCApp::HasLaunchIntent(const std::string &package)
   return GetPackageManager().getLaunchIntentForPackage(package) != NULL;
 }
 
+bool CXBMCApp::StartAppActivity(const std::string &package, const std::string &cls)
+{
+  if (package.empty())
+  {
+    CLog::Log(LOGERROR, "CXBMCApp::StartAppActivity - No package");
+    return false;
+  }
+
+  if (cls.empty())
+  {
+    CLog::Log(LOGWARNING, "CXBMCApp::StartAppActivity - No class, using StartActivity");
+    return StartActivity(package);
+  }
+
+  CJNIComponentName name(package, cls);
+  CJNIIntent newIntent(CJNIIntent::ACTION_MAIN);
+
+  newIntent.addCategory(CJNIIntent::CATEGORY_LAUNCHER);
+  newIntent.setFlags(CJNIIntent::FLAG_ACTIVITY_NEW_TASK | CJNIIntent::FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+  newIntent.setComponent(name);
+
+  startActivity(newIntent);
+  if (xbmc_jnienv()->ExceptionCheck())
+  {
+    CLog::Log(LOGERROR, "CXBMCApp::StartAppActivity - ExceptionOccurred launching %s", package.c_str());
+    xbmc_jnienv()->ExceptionClear();
+    return false;
+  }
+
+  return true;
+}
+
 // Note intent, dataType, dataURI all default to ""
 bool CXBMCApp::StartActivity(const std::string &package, const std::string &intent, const std::string &dataType, const std::string &dataURI)
 {
+  CLog::Log(LOGDEBUG, "StartActivity: p:%s / i:%s / t:%s / u:%s", package.c_str(), intent.c_str(), dataType.c_str(), dataURI.c_str());
   CJNIIntent newIntent;
   if (intent.empty())
   {

MrMC/mrmc

xbmc/platform/android/activity/XBMCApp.cpp
@@ -85,6 +85,8 @@
 #include <androidjni/Display.h>
 #include <androidjni/BitmapFactory.h>
 #include <androidjni/SystemClock.h>
+#include <androidjni/ComponentName.h>
+#include <androidjni/ResolveInfo.h>
 
 #include "platform/android/activity/AndroidKey.h"
 #include "AndroidFeatures.h"
@@ -921,24 +923,52 @@ std::vector<androidPackage> CXBMCApp::GetApplications()
   CSingleLock lock(m_applicationsMutex);
   if (m_applications.empty())
   {
-    CJNIList<CJNIApplicationInfo> packageList = GetPackageManager().getInstalledApplications(CJNIPackageManager::GET_ACTIVITIES);
-    int numPackages = packageList.size();
+    std::map<std::string, androidPackage> applications;
+    CJNIIntent main(CJNIIntent::ACTION_MAIN, CJNIURI());
+
+    if (CAndroidFeatures::IsLeanback())  // First try leanback
+    {
+      main.addCategory(CJNIIntent::CATEGORY_LEANBACK_LAUNCHER);
+
+      CJNIList<CJNIResolveInfo> launchables = GetPackageManager().queryIntentActivities(main, 0);
+      int numPackages = launchables.size();
+      for (int i = 0; i < numPackages; i++)
+      {
+        CJNIResolveInfo launchable = launchables.get(i);
+        CJNIActivityInfo activity = launchable.activityInfo;
+
+        androidPackage newPackage;
+        newPackage.packageName = activity.applicationInfo.packageName;
+        newPackage.className = activity.name;
+        newPackage.packageLabel = launchable.loadLabel(GetPackageManager()).toString();
+        newPackage.icon = activity.applicationInfo.icon;
+        applications.insert(std::make_pair(newPackage.packageName, newPackage));
+      }
+    }
+
+    main.removeCategory(CJNIIntent::CATEGORY_LEANBACK_LAUNCHER);
+    main.addCategory(CJNIIntent::CATEGORY_LAUNCHER);
+
+    CJNIList<CJNIResolveInfo> launchables = GetPackageManager().queryIntentActivities(main, 0);
+    int numPackages = launchables.size();
     for (int i = 0; i < numPackages; i++)
     {
-      CJNIIntent intent;
-      if (CAndroidFeatures::IsLeanback())
-        intent = GetPackageManager().getLeanbackLaunchIntentForPackage(packageList.get(i).packageName);
-      if (!intent)
-        intent = GetPackageManager().getLaunchIntentForPackage(packageList.get(i).packageName);
-      if (!intent)
-        continue;
+      CJNIResolveInfo launchable = launchables.get(i);
+      CJNIActivityInfo activity = launchable.activityInfo;
 
-      androidPackage newPackage;
-      newPackage.packageName = packageList.get(i).packageName;
-      newPackage.packageLabel = GetPackageManager().getApplicationLabel(packageList.get(i)).toString();
-      newPackage.icon = packageList.get(i).icon;
-      m_applications.push_back(newPackage);
+      if (applications.find(activity.applicationInfo.packageName) == applications.end())
+      {
+        androidPackage newPackage;
+        newPackage.packageName = activity.applicationInfo.packageName;
+        newPackage.className = activity.name;
+        newPackage.packageLabel = launchable.loadLabel(GetPackageManager()).toString();
+        newPackage.icon = activity.applicationInfo.icon;
+        applications.insert(std::make_pair(newPackage.packageName, newPackage));
+      }
     }
+
+    for(auto it : applications)
+      m_applications.push_back(it.second);
   }
 
   return m_applications;
@@ -949,9 +979,35 @@ bool CXBMCApp::HasLaunchIntent(const string &package)
   return GetPackageManager().getLaunchIntentForPackage(package) != NULL;
 }
 
+bool CXBMCApp::StartAppActivity(const std::string &package, const std::string &cls)
+{
+  CJNIComponentName name(package, cls);
+  CJNIIntent newIntent(CJNIIntent::ACTION_MAIN);
+
+  newIntent.addCategory(CJNIIntent::CATEGORY_LAUNCHER);
+  newIntent.setFlags(CJNIIntent::FLAG_ACTIVITY_NEW_TASK | CJNIIntent::FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+  newIntent.setComponent(name);
+
+  startActivity(newIntent);
+  if (xbmc_jnienv()->ExceptionCheck())
+  {
+    CLog::Log(LOGERROR, "CXBMCApp::StartActivity - ExceptionOccurred launching %s", package.c_str());
+    xbmc_jnienv()->ExceptionClear();
+    return false;
+  }
+
+  return true;
+}
+
 // Note intent, dataType, dataURI all default to ""
 bool CXBMCApp::StartActivity(const string &package, const string &intent, const string &dataType, const string &dataURI)
 {
+  if (package.find('/') != std::string::npos)
+  {
+    std::vector<std::string> split = StringUtils::Split(package, '/');
+    return StartAppActivity(split[0], split[1]);
+  }
+
   CJNIIntent newIntent;
   if (intent.empty())
   {