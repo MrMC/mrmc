MrMC/mrmc

commit e671820f441a282dfe11a1fa84f66b46c49dfaba

CHG: only double framerate if material is interlaced

xbmc/cores/dvdplayer/DVDCodecs/Video/DVDVideoCodecVideoToolBox.cpp
@@ -203,10 +203,10 @@ bool CDVDVideoCodecVideoToolBox::Open(CDVDStreamInfo &hints, CDVDCodecOptions &o
       memcpy(hints.extradata, saved_extradata, hints.extrasize);
 
       if (interlaced)
-        hints.maybe_interlaced = true;
+        hints.maybe_interlaced = 1;
     }
 
-    if (hints.maybe_interlaced)
+    if (hints.maybe_interlaced > 1)
     {
       CLog::Log(LOGNOTICE, "%s - interlaced content.", __FUNCTION__);
       return false;
@@ -214,7 +214,7 @@ bool CDVDVideoCodecVideoToolBox::Open(CDVDStreamInfo &hints, CDVDCodecOptions &o
 
     m_hints = hints;
     m_options = options;
- 
+
     switch(m_hints.profile)
     {
       //case FF_PROFILE_H264_HIGH_10:
@@ -235,7 +235,7 @@ bool CDVDVideoCodecVideoToolBox::Open(CDVDStreamInfo &hints, CDVDCodecOptions &o
         __FUNCTION__, m_hints.width, m_hints.height);
       return false;
     }
-    
+
     switch (m_hints.codec)
     {
       default:
@@ -390,7 +390,7 @@ void CDVDVideoCodecVideoToolBox::Dispose()
     m_videobuffer.cvBufferRef = nullptr;
     m_videobuffer.iFlags = 0;
   }
-  
+
   while (m_queue_depth)
     DisplayQueuePop();
 }
@@ -459,7 +459,7 @@ int CDVDVideoCodecVideoToolBox::Decode(uint8_t* pData, int iSize, double dts, do
       CLog::Log(LOGNOTICE, "%s - CreateSampleBufferFrom failed", __FUNCTION__);
       return VC_ERROR;
     }
-    
+
     VTDecodeFrameFlags decoderFlags = 0;
     if (m_enable_temporal_processing)
       decoderFlags |= kVTDecodeFrame_EnableTemporalProcessing;
@@ -533,7 +533,7 @@ void CDVDVideoCodecVideoToolBox::Reset(void)
 
   while (m_queue_depth)
     DisplayQueuePop();
-  
+
   m_codecControlFlags = 0;
 }
 
@@ -600,7 +600,7 @@ bool CDVDVideoCodecVideoToolBox::GetPicture(DVDVideoPicture* pDvdVideoPicture)
     CLog::Log(LOGDEBUG, "%s - VTBDecoderDecode dts(%f), pts(%f), old_pts(%f)", __FUNCTION__,
       pDvdVideoPicture->dts, pDvdVideoPicture->pts, old_pts);
   old_pts = pDvdVideoPicture->pts;
-  
+
 //  CLog::Log(LOGDEBUG, "%s - VTBDecoderDecode dts(%f), pts(%f), cvBufferRef(%p)", __FUNCTION__,
 //    pDvdVideoPicture->dts, pDvdVideoPicture->pts, pDvdVideoPicture->cvBufferRef);
 
@@ -1214,10 +1214,10 @@ CDVDVideoCodecVideoToolBox::DestroyVTSession()
 typedef void (*VTDecompressionOutputCallback)(
 		void * CM_NULLABLE decompressionOutputRefCon,
 		void * CM_NULLABLE sourceFrameRefCon,
-		OSStatus status, 
+		OSStatus status,
 		VTDecodeInfoFlags infoFlags,
 		CM_NULLABLE CVImageBufferRef imageBuffer,
-		CMTime presentationTimeStamp, 
+		CMTime presentationTimeStamp,
 		CMTime presentationDuration );
 
 void
@@ -1269,7 +1269,7 @@ CDVDVideoCodecVideoToolBox::VTDecoderCallback(
   {
     newFrame->width  = CVPixelBufferGetWidth(imageBuffer);
     newFrame->height = CVPixelBufferGetHeight(imageBuffer);
-  }  
+  }
   newFrame->pixel_buffer_format = CVPixelBufferGetPixelFormatType(imageBuffer);;
   newFrame->pixel_buffer_ref = CVBufferRetain(imageBuffer);
   newFrame->pts = (double)pts.value;

xbmc/cores/VideoRenderers/BaseRenderer.cpp
@@ -54,7 +54,7 @@ CBaseRenderer::CBaseRenderer()
     m_rotatedDestCoords[i].x = 0;
     m_rotatedDestCoords[i].y = 0;
     m_savedRotatedDestCoords[i].x = 0;
-    m_savedRotatedDestCoords[i].y = 0;    
+    m_savedRotatedDestCoords[i].y = 0;
   }
 
   m_RenderUpdateCallBackFn = NULL;
@@ -281,9 +281,9 @@ RESOLUTION CBaseRenderer::FindClosestResolution(float fps, float multiplier, RES
 
     int i_weight = MathUtils::round_int(RefreshWeight(info.fRefreshRate, fRefreshRate * multiplier) * 10000.0);
 
-    // Closer the better, prefer higher refresh rate if the same
+    // Closer the better, prefer lower refresh rate if the same
     if ((i_weight < c_weight) ||
-        (i_weight == c_weight && info.fRefreshRate > curr.fRefreshRate && loop_diff <= curr_diff) ||
+        (i_weight == c_weight && info.fRefreshRate < curr.fRefreshRate && loop_diff <= curr_diff) ||
         (i_weight == c_weight && info.fRefreshRate == curr.fRefreshRate && loop_diff < curr_diff))
     {
       current = (RESOLUTION)i;
@@ -371,7 +371,7 @@ inline void CBaseRenderer::ReorderDrawPoints()
   int diffY = 0;
   int centerX = 0;
   int centerY = 0;
-  
+
   if (changeAspect)// we are either rotating by 90 or 270 degrees which inverts aspect ratio
   {
     int newWidth = m_destRect.Height(); // new width is old height
@@ -398,7 +398,7 @@ inline void CBaseRenderer::ReorderDrawPoints()
         newWidth /= aspectRatio;
       }
     }
-    
+
     // calculate the center point of the view
     centerX = m_viewRect.x1 + m_viewRect.Width() / 2;
     centerY = m_viewRect.y1 + m_viewRect.Height() / 2;
@@ -409,7 +409,7 @@ inline void CBaseRenderer::ReorderDrawPoints()
     // calculate the number of pixels we need to go in each
     // y direction from the center point
     diffY = newHeight / 2;
-    
+
   }
 
   for (int destIdx=0, srcIdx=pointOffset; destIdx < 4; destIdx++)
@@ -453,13 +453,13 @@ void CBaseRenderer::saveRotatedCoords()
 void CBaseRenderer::syncDestRectToRotatedPoints()
 {
   m_rotatedDestCoords[0].x = m_destRect.x1;
-  m_rotatedDestCoords[0].y = m_destRect.y1;  
+  m_rotatedDestCoords[0].y = m_destRect.y1;
   m_rotatedDestCoords[1].x = m_destRect.x2;
   m_rotatedDestCoords[1].y = m_destRect.y1;
   m_rotatedDestCoords[2].x = m_destRect.x2;
-  m_rotatedDestCoords[2].y = m_destRect.y2;  
+  m_rotatedDestCoords[2].y = m_destRect.y2;
   m_rotatedDestCoords[3].x = m_destRect.x1;
-  m_rotatedDestCoords[3].y = m_destRect.y2; 
+  m_rotatedDestCoords[3].y = m_destRect.y2;
 }
 
 void CBaseRenderer::restoreRotatedCoords()

xbmc/cores/dvdplayer/DVDCodecs/Video/DVDVideoCodecAndroidMediaCodec.cpp
@@ -537,7 +537,7 @@ bool CDVDVideoCodecAndroidMediaCodec::Open(CDVDStreamInfo &hints, CDVDCodecOptio
       break;
   }
 
-  if (hints.maybe_interlaced)
+  if (hints.maybe_interlaced > 0)
   {
     CLog::Log(LOGDEBUG, "CDVDVideoCodecAndroidMediaCodec::Open - possible interlaced codec(%s%s), profile(%d), level(%d)",
       m_formatname.c_str(), m_render_surface ? "-sfc" : "-egl", hints.profile, hints.level);
@@ -692,7 +692,7 @@ bool CDVDVideoCodecAndroidMediaCodec::Open(CDVDStreamInfo &hints, CDVDCodecOptio
     // for FireOS, MediaCodec returns the interlaced frames as is,
     // for Shield, MediaCodec will kick in an internal deinterlacer
     // and return progressive frames.
-    if (hints.maybe_interlaced)
+    if (hints.maybe_interlaced > 0)
     {
       m_videobuffer.iFlags |= DVP_FLAG_INTERLACED;
       // should be DVP_FLAG_TOP_FIELD_FIRST but android seems to be inverted ?

xbmc/cores/dvdplayer/DVDDemuxers/DVDDemux.cpp
@@ -220,6 +220,9 @@ void CDemuxStream::CheckForInterlaced(const AVCodecParserContext *parser)
   if (vstream == nullptr)
     return;
 
+  if (vstream->bMaybeInterlaced > -1) // we already came to a conclusion
+    return;
+
   if (parser)
   {
     switch(parser->field_order)
@@ -227,21 +230,68 @@ void CDemuxStream::CheckForInterlaced(const AVCodecParserContext *parser)
       default:
       case AV_FIELD_PROGRESSIVE:
         // default value for bMaybeInterlaced but we set it anyway
-        vstream->bMaybeInterlaced = false;
+        vstream->bMaybeInterlaced = 0;
         break;
       case AV_FIELD_TT: //< Top coded_first, top displayed first
       case AV_FIELD_BB: //< Bottom coded first, bottom displayed first
       case AV_FIELD_TB: //< Top coded first, bottom displayed first
       case AV_FIELD_BT: //< Bottom coded first, top displayed first
-        vstream->bMaybeInterlaced = true;
+        vstream->bMaybeInterlaced = 1;
         break;
       case AV_FIELD_UNKNOWN:
-        {
-          // if picture_structure is AV_PICTURE_STRUCTURE_UNKNOWN, no clue so assume progressive
-          bool interlaced = parser->picture_structure == AV_PICTURE_STRUCTURE_TOP_FIELD;
-          interlaced |= parser->picture_structure == AV_PICTURE_STRUCTURE_BOTTOM_FIELD;
-          vstream->bMaybeInterlaced = interlaced;
-        }
+      {
+        // if picture_structure is AV_PICTURE_STRUCTURE_UNKNOWN, no clue so assume progressive
+        bool interlaced = parser->picture_structure == AV_PICTURE_STRUCTURE_TOP_FIELD;
+        interlaced |= parser->picture_structure == AV_PICTURE_STRUCTURE_BOTTOM_FIELD;
+        vstream->bMaybeInterlaced = (interlaced ? 1 : 0);
+      }
+        break;
+    }
+  }
+}
+
+void CDemuxStream::CheckForInterlaced(const AVStream *stream)
+{
+  CDemuxStreamVideo *vstream = dynamic_cast<CDemuxStreamVideo*>(this);
+  // paranoid check to make sure we are a video stream
+  if (vstream == nullptr)
+    return;
+
+  if (vstream->bMaybeInterlaced > -1) // we already came to a conclusion
+    return;
+
+  if (stream->parser)
+  {
+    CheckForInterlaced(stream->parser);
+    return;
+  }
+  else
+  {
+    AVCodecContext *avctx = avcodec_alloc_context3(NULL);
+    if (!avctx)
+      return;
+
+    int ret = avcodec_parameters_to_context(avctx, stream->codecpar);
+    if (ret < 0)
+    {
+      avcodec_free_context(&avctx);
+      return;
+    }
+
+    switch(avctx->field_order)
+    {
+      default:
+      case AV_FIELD_PROGRESSIVE:
+        vstream->bMaybeInterlaced = 0;
+        break;
+      case AV_FIELD_TT: //< Top coded_first, top displayed first
+      case AV_FIELD_BB: //< Bottom coded first, bottom displayed first
+      case AV_FIELD_TB: //< Top coded first, bottom displayed first
+      case AV_FIELD_BT: //< Bottom coded first, top displayed first
+        vstream->bMaybeInterlaced = 1;
+        break;
+      case AV_FIELD_UNKNOWN:
+        // No clue, yet
         break;
     }
   }

xbmc/cores/dvdplayer/DVDDemuxers/DVDDemux.h
@@ -113,6 +113,7 @@ public:
   virtual void      SetDiscard(AVDiscard discard);
   virtual AVDiscard GetDiscard();
   virtual void CheckForInterlaced(const AVCodecParserContext *parser);
+  virtual void CheckForInterlaced(const AVStream *stream);
 
   int iId;         // most of the time starting from 0
   int iPhysicalId; // id
@@ -135,7 +136,7 @@ public:
   int  changes; // increment on change which player may need to know about
 
   enum EFlags
-  { FLAG_NONE             = 0x0000 
+  { FLAG_NONE             = 0x0000
   , FLAG_DEFAULT          = 0x0001
   , FLAG_DUB              = 0x0002
   , FLAG_ORIGINAL         = 0x0004
@@ -170,7 +171,7 @@ public:
     iColorRange = 0;
     iColorTransfer = 0;
     iColorPrimaries = 0;
-    bMaybeInterlaced = false;
+    bMaybeInterlaced = -1;
   }
 
   virtual ~CDemuxStreamVideo() {}
@@ -191,7 +192,7 @@ public:
   int iColorTransfer;
   int iColorPrimaries;
   std::string stereo_mode; // expected stereo mode
-  bool bMaybeInterlaced; // stream 'might' be interlaced, on some, we can't tell for sure unless decoding.
+  int bMaybeInterlaced; // stream 'might' be interlaced, on some, we can't tell for sure unless decoding.
 };
 
 class CDemuxStreamAudio : public CDemuxStream

xbmc/cores/dvdplayer/DVDDemuxers/DVDDemuxFFmpeg.cpp
@@ -264,7 +264,7 @@ bool CDVDDemuxFFmpeg::Open(CDVDInputStream* pInput, bool streaminfo, bool filein
       {
         url.SetProtocol("mmst");
         strFile = url.Get();
-      } 
+      }
     }
  #if 0
     if (url.IsFileType("m3u8"))
@@ -446,9 +446,9 @@ bool CDVDDemuxFFmpeg::Open(CDVDInputStream* pInput, bool streaminfo, bool filein
   }
 
   // Avoid detecting framerate if advancedsettings.xml says so
-  if (g_advancedSettings.m_videoFpsDetect == 0) 
+  if (g_advancedSettings.m_videoFpsDetect == 0)
       m_pFormatContext->fps_probe_size = 0;
-  
+
   // analyse very short to speed up mjpeg playback start
   if (iformat && (strcmp(iformat->name, "mjpeg") == 0) && m_ioContext->seekable == 0)
     av_opt_set_int(m_pFormatContext, "analyzeduration", 500000, 0);
@@ -987,6 +987,7 @@ DemuxPacket* CDVDDemuxFFmpeg::Read()
         // content has changed
         stream = AddStream(pPacket->iStreamId);
       }
+      pPacket->interlaced = ((CDemuxStreamVideo*)stream)->bMaybeInterlaced;
     }
     if (!stream)
     {
@@ -1094,7 +1095,7 @@ bool CDVDDemuxFFmpeg::SeekByte(int64_t pos)
 void CDVDDemuxFFmpeg::UpdateCurrentPTS()
 {
   m_currentPts = DVD_NOPTS_VALUE;
-  
+
   int idx = av_find_default_stream_index(m_pFormatContext);
   if (idx >= 0)
   {
@@ -1262,7 +1263,7 @@ CDemuxStream* CDVDDemuxFFmpeg::AddStream(int iId)
         st->iChannelLayout = pStream->codec->channel_layout;
         if (st->iBitsPerSample == 0)
           st->iBitsPerSample = pStream->codec->bits_per_coded_sample;
-	
+
         if(av_dict_get(pStream->metadata, "title", NULL, 0))
           st->m_description = av_dict_get(pStream->metadata, "title", NULL, 0)->value;
 
@@ -1346,10 +1347,10 @@ CDemuxStream* CDVDDemuxFFmpeg::AddStream(int iId)
         st->iColorRange = pStream->codec->color_range;
         st->iColorTransfer = pStream->codec->color_trc;
         st->iColorPrimaries = pStream->codec->color_primaries;
-        st->CheckForInterlaced(pStream->parser);
+        st->CheckForInterlaced(pStream);
 
         AVDictionaryEntry *rtag = av_dict_get(pStream->metadata, "rotate", NULL, 0);
-        if (rtag) 
+        if (rtag)
           st->iOrientation = atoi(rtag->value);
 
         // detect stereoscopic mode
@@ -1360,7 +1361,7 @@ CDemuxStream* CDVDDemuxFFmpeg::AddStream(int iId)
         if (!stereoMode.empty())
           st->stereo_mode = stereoMode;
 
-        
+
         if ( m_pInput->IsStreamType(DVDSTREAM_TYPE_DVD) )
         {
           if (pStream->codec->codec_id == AV_CODEC_ID_PROBE)
@@ -1425,10 +1426,10 @@ CDemuxStream* CDVDDemuxFFmpeg::AddStream(int iId)
         {
           CDemuxStreamSubtitleFFmpeg* st = new CDemuxStreamSubtitleFFmpeg(this, pStream);
           stream = st;
-	    
+
           if(av_dict_get(pStream->metadata, "title", NULL, 0))
             st->m_description = av_dict_get(pStream->metadata, "title", NULL, 0)->value;
-	
+
           break;
         }
       }
@@ -1611,7 +1612,7 @@ void CDVDDemuxFFmpeg::GetChapterName(std::string& strChapterName, int chapterIdx
   if (chapterIdx <= 0 || chapterIdx > GetChapterCount())
     chapterIdx = GetChapter();
   CDVDInputStream::IChapter* ich = dynamic_cast<CDVDInputStream::IChapter*>(m_pInput);
-  if(ich)  
+  if(ich)
     ich->GetChapterName(strChapterName, chapterIdx);
   else
   {
@@ -1633,7 +1634,7 @@ int64_t CDVDDemuxFFmpeg::GetChapterPos(int chapterIdx)
     return 0;
 
   CDVDInputStream::IChapter* ich = dynamic_cast<CDVDInputStream::IChapter*>(m_pInput);
-  if(ich)  
+  if(ich)
     return ich->GetChapterPos(chapterIdx);
 
   return m_pFormatContext->chapters[chapterIdx-1]->start*av_q2d(m_pFormatContext->chapters[chapterIdx-1]->time_base);
@@ -1882,10 +1883,6 @@ void CDVDDemuxFFmpeg::ParsePacket(AVPacket *pkt)
       {
         st->codec->extradata_size = 0;
       }
-      // check for interlaced content, the check in AddStream might
-      // be skipped if parser is null, we do it in both places
-      if (stream)
-        stream->CheckForInterlaced(st->parser);
     }
   }
 
@@ -1927,6 +1924,11 @@ void CDVDDemuxFFmpeg::ParsePacket(AVPacket *pkt)
     avcodec_decode_video2(st->codec, &picture, &got_picture, pkt);
     av_frame_unref(&picture);
   }
+
+  // check for interlaced content, the check in AddStream might
+  // be skipped if parser is null, we do it in both places
+  if (stream)
+    stream->CheckForInterlaced(st);
 }
 
 bool CDVDDemuxFFmpeg::IsVideoReady()

xbmc/cores/dvdplayer/DVDDemuxers/DVDDemuxPacket.h
@@ -33,4 +33,5 @@
   double pts; // pts in DVD_TIME_BASE
   double dts; // dts in DVD_TIME_BASE
   double duration; // duration in DVD_TIME_BASE if available
+  int interlaced;  // is packet interlaced
 } DemuxPacket;

xbmc/cores/dvdplayer/DVDPlayerVideo.cpp
@@ -438,6 +438,7 @@ void CDVDPlayerVideo::Process()
       DemuxPacket* pPacket = ((CDVDMsgDemuxerPacket*)pMsg)->GetPacket();
       bool bPacketDrop     = ((CDVDMsgDemuxerPacket*)pMsg)->GetPacketDrop();
 
+      m_hints.maybe_interlaced = pPacket->interlaced;
       if (m_stalled)
       {
         CLog::Log(LOGINFO, "CDVDPlayerVideo - Stillframe left, switching to normal playback");
@@ -880,6 +881,16 @@ int CDVDPlayerVideo::OutputPicture(const DVDVideoPicture* src, double pts)
 
 #ifdef HAS_VIDEO_PLAYBACK
   double config_framerate = m_bFpsInvalid ? 0.0 : m_fFrameRate;
+  if (m_hints.maybe_interlaced == 1)
+  {
+    CLog::Log(LOGDEBUG, "%s - interlaced: %s", __FUNCTION__,
+              m_hints.maybe_interlaced ? "yes" : "no");
+
+    if (MathUtils::FloatEquals(config_framerate, 25.0, 0.02))
+      config_framerate = 50.0;
+    else if (MathUtils::FloatEquals(config_framerate, 29.97, 0.02))
+      config_framerate = 59.94;  }
+
   double render_framerate = g_graphicsContext.GetFPS();
   // disable switching if running video splash
   int settingAdjustRefreshRate = CSettings::GetInstance().GetInt(CSettings::SETTING_VIDEOPLAYER_ADJUSTREFRESHRATE);

commit 3d4652cb73dbae510f891dd0494f31817b1709dd

fixed, regression from e671820f441 (CHG: only double framerate if material is interlaced)

xbmc/cores/dvdplayer/DVDCodecs/Video/DVDVideoCodecVideoToolBox.cpp
@@ -162,7 +162,7 @@ bool CDVDVideoCodecVideoToolBox::Open(CDVDStreamInfo &hints, CDVDCodecOptions &o
         hints.maybe_interlaced = 1;
     }
 
-    if (hints.maybe_interlaced > 1)
+    if (hints.maybe_interlaced > 0)
     {
       CLog::Log(LOGNOTICE, "%s - interlaced content.", __FUNCTION__);
       return false;