cc29fdafa3d267c86df98687894f92ef398d6fbb
S. Davilla <davilla@4pi.com>
2019-10-22 09:07:56 -0400

    fix: since we might check for interlaced earler, qualify before using the 2nd check

--- xbmc/cores/dvdplayer/DVDCodecs/Video/DVDVideoCodecVideoToolBox.cpp
+++ xbmc/cores/dvdplayer/DVDCodecs/Video/DVDVideoCodecVideoToolBox.cpp
@@ -158,7 +158,10 @@ bool CDVDVideoCodecVideoToolBox::Open(CDVDStreamInfo &hints, CDVDCodecOptions &o
       // restore original extradata contents
       memcpy(hints.extradata, saved_extradata, hints.extrasize);
 
-      if (interlaced)
+      // if maybe_interlaced is already set ( > 0 )
+      // assume who set it is smarter than us :)
+      // and ignore parseh264 results for interlaced
+      if (hints.maybe_interlaced == -1 && interlaced)
         hints.maybe_interlaced = 1;
     }
 
